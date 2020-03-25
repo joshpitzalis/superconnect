b48435534643dcd6efc45947059ec2c15c3a2e0a
Joshua Pittman <joshpitzalis@gmail.com>
2020-03-25 11:37:27 +0530

    extension working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
+/* eslint-disable*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+InboxSDK.load(2, 'sdk_superconnect_af254a2366').then(({Global}) => {
+
+  // Compose.registerComposeViewHandler((composeView) => {
+
+  //   composeView.addButton({
+  //     title: "My Nifty Button!",
+  //     iconUrl: 'http://tachyons.io/img/avatar-jasonli.jpg',
+  //     hasDropdown: true,
+  //     onClick(event) {
+  //       ReactDOM.render( <App />, event.dropdown.el);
+
+  //       event.dropdown.once('destroy', () => (
+  //         ReactDOM.unmountComponentAtNode(event.dropdown.el)
+  //       ));
+  //     }
+  //   });
+  // });
+
+  let el = document.createElement("div");
+	ReactDOM.render(<App />, el);
+
+  Global.addSidebarContentPanel({
+    title: "Superconnect",
+    iconUrl:'http://tachyons.io/img/avatar-jasonli.jpg'	,
+    el
+  })
+});
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
